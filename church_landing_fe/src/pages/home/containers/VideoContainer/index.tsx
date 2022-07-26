f2b83e221afea01b6644e97a63e91b6b23ee6fa0
Akhil Krishnan <akhilkrishnan.freelancer@gmail.com>
2022-07-26 11:24:12 +0000

    Embed layout

---
+++ church_landing_fe/src/pages/home/containers/VideoContainer/index.tsx
@@ -0,0 +1,26 @@
+import { Grid, Typography } from '@material-ui/core'
+import React from 'react'
+import VideoComponent from '../../components/VideoComponent'
+
+const VideoContainer = () => {
+    return (
+        <Grid container direction="row" justifyContent="center" alignItems="flex-start">
+            <Grid item lg={6} xs={11}>
+                <Grid container spacing={5} direction="row" justifyContent="center" >
+                    <Grid item xs={12} lg={6}>
+                        <VideoComponent />
+                    </Grid>
+                    <Grid item xs={12} lg={6} >
+                        <Typography variant="subtitle2" align="justify"><b>Welcome</b></Typography>
+                        <Typography variant="h5" align="left"><b>Teaching the word of god with simplicity and understanding is our goal</b></Typography>
+                        <br />
+                        <Typography variant="subtitle1" align="justify">Believe that the Bible is the written and inspired Word of God. Their purpose is to invoke change in the lives of people across the globe. Ministry’s desire and vision is for all to hear the Word and gain understanding of God’s love and promises.</Typography>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
+
+    )
+}
+
+export default VideoContainer
--- church_landing_fe/src/pages/home/index.tsx
+++ church_landing_fe/src/pages/home/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import SiteAppBar from '../../common/components/AppBar'
 import HomeCarousel from './components/HomeCarousel'
+import VideoContainer from './containers/VideoContainer'
 
 export const HomePage = () => {
     return (
@@ -8,6 +9,7 @@ export const HomePage = () => {
             <SiteAppBar >
                 <>
                     <HomeCarousel />
+                    <VideoContainer/>
                 </>
             </SiteAppBar>
 
